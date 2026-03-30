a5d9ef03bf9dfa18d60b1c4f000a4bf82fa7d290
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-30 19:30:15 +0300

    Create RegisterRequest DTO with email and password validation

---
+++ src/main/java/com/bookingsystem/user_service/dto/request/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.bookingsystem.user_service.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RegisterRequest {
+
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+}
