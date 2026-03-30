93eb59061e8daf30ff16035b11fa504329ab87e2
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-30 19:30:48 +0300

    Add JwtService utility for token generation in User Service

---
+++ src/main/java/com/bookingsystem/user_service/util/JwtService.java
@@ -0,0 +1,22 @@
+package com.bookingsystem.user_service.util;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+
+import java.util.Date;
+
+public class JwtService {
+
+    private final String SECRET = "my-secret-key-123456";
+
+    public String generateToken(String userId, String role) {
+
+        return Jwts.builder()
+                .setSubject(userId)
+                .claim("role", role)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 86400000))
+                .signWith(Keys.hmacShaKeyFor(SECRET.getBytes()))
+                .compact();
+    }
+}
