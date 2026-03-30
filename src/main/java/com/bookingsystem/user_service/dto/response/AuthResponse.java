6f03863f9e2dd8af2d5d326bfad5952d78d6fe65
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-30 19:30:40 +0300

    Create AuthResponse DTO with token field and constructor

---
+++ src/main/java/com/bookingsystem/user_service/dto/response/AuthResponse.java
@@ -0,0 +1,13 @@
+package com.bookingsystem.user_service.dto.response;
+
+import lombok.Getter;
+
+@Getter
+public class AuthResponse {
+
+    private String token;
+
+    public AuthResponse(String token) {
+        this.token = token;
+    }
+}
